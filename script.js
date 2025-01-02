ca656825856a0d0090519b283284b294b80dc06c
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-02 17:47:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function toCase(text) {
   // write your code here
+	let case1 = text.toLowerCase();
+	let case2 = text.toUpperCase();
+
+	return case1 + "-"; + case2;
 }
 
 // DO not change the code below
